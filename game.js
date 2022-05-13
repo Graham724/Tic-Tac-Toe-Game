0b0296c564864e76f48de3be13052a933e23087b
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-05-13 18:52:14 -0500

    styles update

---
+++ game.js
@@ -0,0 +1,17 @@
+let board;
+let playerO;
+let currentPlayer = playerO;
+let gameOver = false;
+
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [
+        [' ',' ', ' '],
+        [' ',' ', ' '],
+        [' ',' ', ' ']
+    ]
+}
