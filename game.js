3befdcecc6af5394bf2fee05b15d6716ec27248a
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-05-13 19:39:29 -0500

    game functional

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let board;
-let playerO;
+let playerO = 'O';
+let playerX = 'X';
 let currentPlayer = playerO;
 let gameOver = false;
 
@@ -27,7 +28,85 @@ function setGame() {
             if(c == 0 || c == 1) {
                 tile.classList.add('vertical-line')
             }
+            tile.addEventListener('click', setTile);
             document.getElementById('board').append(tile);
         }
+    }
+}
+
+function setTile() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split('-')
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    if (board[r][c] != ' ') {
+        return;
+    }
+
+    board[r][c] = currentPlayer;
+    this.innerText = currentPlayer;
+
+    if (currentPlayer == playerO) {
+        currentPlayer = playerX;
+    }
+    else {
+        currentPlayer = playerO;
+    }
+
+    checkWinner();
+}
+
+function checkWinner() {
+    //horizontal check
+    for(let r = 0; r < 3; r++) {
+        if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != ' ') {
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(r.toString() + '-' + i.toString());
+                tile.classList.add('winner');
+            }
+            gameOver = true;
+            return;
+        }
+    }
+
+    // vertical check
+    for(let c = 0; c < 3; c++) {
+        if (board[0][c] == board[1][c] && board[1][c] == board[2][c] && board[0][c] != ' ') {
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(i.toString() + '-' + c.toString());
+                tile.classList.add('winner');
+            }
+            gameOver = true;
+            return;
+        }
+    }
+
+    // diagonal check
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        for (let i = 0; i < 3; i++) {
+            let tile = document.getElementById(i.toString() + '-' + i.toString());
+            tile.classList.add('winner');
+        }
+        gameOver = true;
+        return;
+    }
+
+    // anti diagonal check
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+        let tile = document.getElementById('0-2');
+        tile.classList.add('winner');
+
+        tile = document.getElementById('1-1');
+        tile.classList.add('winner');
+
+        tile = document.getElementById('2-0');
+        tile.classList.add('winner');
+
+        gameOver = true;
+        return;
     }
 }
